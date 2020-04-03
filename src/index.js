ab5bd674f28c106351981f11df663d4974fc138c
Yan Wang <ywang@talend.com>
2020-04-03 21:44:47 +0800

    closure in Module pattern

--- src/index.js
+++ src/index.js
@@ -7,19 +7,36 @@ function useState(initialValue) {
   }
   return [_val, setState];
 }
-var [foo, setFoo] = useState(0);
-console.log(foo); // logs 0 without needing function call
-setFoo(1); // sets _val inside useState's scope
-console.log(foo); // logs 0 - oops!!
+// var [foo, setFoo] = useState(0);
+// console.log(foo); // logs 0 without needing function call
+// setFoo(1); // sets _val inside useState's scope
+// console.log(foo); // logs 0 - oops!!
 
+const MyReact = (function() {
+  let _val;
+  return {
+    render: Component => {
+      const Comp = Component();
+      Comp.render();
+      return Comp;
+    },
+    useState: initialValue => {
+      _val = _val || initialValue;
+      function setState(newState) {
+        _val = newState;
+      }
+      return [_val, setState];
+    }
+  };
+})();
 function Counter() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = MyReact.useState(0);
   return {
     click: () => setCount(count + 1),
     render: () => console.log(count)
   };
 }
-// const counter = Counter();
-// counter.render();
-// counter.click();
-// counter.render();
+let counter;
+counter = MyReact.render(Counter);
+counter.click();
+counter = MyReact.render(Counter);
